c0a270437898bb5013da79cfec68f0b1362fe165
Adriana  Cambronero <cambrussel@Adrianas-MacBook-Pro.local>
2015-06-28 19:21:34 -0600

    Initial Commit

---
+++ Tarea1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tarea1
+//
+//  Created by Adriana  Cambronero on 6/28/15.
+//  Copyright (c) 2015 Adriana Cambronero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tarea1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tarea1
+//
+//  Created by Adriana  Cambronero on 6/28/15.
+//  Copyright (c) 2015 Adriana Cambronero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
